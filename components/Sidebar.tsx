f7a15e58b8cfc85d08858c2fd780a09821327877
Davi <dv.andrade04@gmail.com>
2024-03-18 00:53:05 -0300

    working on flexbox

---
+++ components/Sidebar.tsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+export default function Sidebar() {
+  return (
+    <nav className="mt-20 min-w-min flex flex-col text-sm gap-4">
+      <p className="z-10 ">Início</p>
+      <p className="z-10 ">Projetos</p>
+      <p className="z-10 ">Sobre</p>
+      <p className="z-10 ">Contato</p>
+    </nav>
+  );
+}
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,35 +1,38 @@
 import React from "react";
 import Image from "next/image";
+import Sidebar from "../../components/Sidebar";
 
 export default function Home() {
   return (
     <>
-      <div className="noise"></div>
+      {/* <div className="noise"></div> */}
 
-      <main className="flex h-[89vh] flex-col p-10 m-10 ">
-        <div className="absolute justify-center">
+      <main className="flex flex-col h-[89vh] p-10 m-10 ">
+        {/* <div className="absolute justify-center">
           <div className="bg-shape1 bg-teal opacity-50 bg-blur"></div>
           <div className="bg-shape2 bg-primary opacity-50 bg-blur"></div>
-          {/* <div className="bg-shape1 bg-purple opacity-50 bg-blur"></div> */}
-        </div>
+        </div> */}
 
-        <div className="flex space-between">
+        <header className="flex w-full space-between">
           <div className="z-10 w-full flex-start flex-col font-mono text-sm lg:flex">
-            <p className="fixed left-0 top-0 flex lg:static lg:w-auto text-2xl">Davi Andrade</p>
-            <p className="fixed left-0 top-0 flex lg:static lg:w-auto text-xl">
-              Desenvolvedor FrontEnd
-            </p>
+            <p className=" left-0 top-0 lg:static lg:w-auto text-2xl">Davi Andrade</p>
+            <p className=" left-0 top-0  lg:static lg:w-auto text-xl">Programador FrontEnd</p>
           </div>
-          <div className="flex flex-end">
+          <div className="z-10">
             <Image width={60} height={600} alt="Theme" src={"/dark.png"} />
           </div>
-        </div>
+        </header>
 
-        <div className="z-10 max-w-5xl w-full mt-20 flex-start flex-col font-mono text-sm lg:flex text-lg gap-4">
-          <p className="fixed left-0 top-0 flex lg:static lg:w-auto ">Início</p>
-          <p className="fixed left-0 top-0 flex lg:static lg:w-auto ">Projetos</p>
-          <p className="fixed left-0 top-0 flex lg:static lg:w-auto ">Sobre</p>
-          <p className="fixed left-0 top-0 flex lg:static lg:w-auto ">Contato</p>
+        <Sidebar />
+
+        <div className="z-10 w-full flex justify-end">
+          <article className="font-mono text-sm text-lg">
+            <p className=" w-[150px]">
+              {
+                "Graduando em Análise e Desenvolvimento de Sistemas e, além da faculdade, estudo programação online para aprimorar continuamente minhas habilidades. Antes do meu primeiro 'Hello World', fiz trabalhos voluntários e estive à frente do protagonismo estudantil no ensino médio."
+              }
+            </p>
+          </article>
         </div>
       </main>
     </>
