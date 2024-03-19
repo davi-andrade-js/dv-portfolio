7882775943ee717424c72695bb78ee4ded483626
Davi <dv.andrade04@gmail.com>
2024-03-19 00:10:52 -0300

    switched flex for grid

--- components/Sidebar.tsx
+++ components/Sidebar.tsx
@@ -2,11 +2,13 @@ import React from "react";
 
 export default function Sidebar() {
   return (
-    <nav className="mt-20 min-w-min flex flex-col text-sm gap-4">
-      <p className="z-10 ">Início</p>
-      <p className="z-10 ">Projetos</p>
-      <p className="z-10 ">Sobre</p>
-      <p className="z-10 ">Contato</p>
-    </nav>
+    <div className="grid col-start-1 row-start-2">
+      <nav className="mt-20 grid col-start-1 row-start-2 min-w-min text-sm gap-4">
+        <p className="z-10 ">Início</p>
+        <p className="z-10 ">Projetos</p>
+        <p className="z-10 ">Sobre</p>
+        <p className="z-10 ">Contato</p>
+      </nav>
+    </div>
   );
 }
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -7,26 +7,26 @@ export default function Home() {
     <>
       {/* <div className="noise"></div> */}
 
-      <main className="flex flex-col h-[89vh] p-10 m-10 ">
-        {/* <div className="absolute justify-center">
+      <main className="grid grid-cols-4 grid-rows-4 h-[89vh] p-10 m-10 ">
+        <div className="motion-area justify-center">
           <div className="bg-shape1 bg-teal opacity-50 bg-blur"></div>
           <div className="bg-shape2 bg-primary opacity-50 bg-blur"></div>
-        </div> */}
+        </div>
 
-        <header className="flex w-full space-between">
-          <div className="z-10 w-full flex-start flex-col font-mono text-sm lg:flex">
+        <header className="grid justify-between w-full col-start-1 row-start-1 col-end-5 row-end-2">
+          <div className="z-10 flex flex-col font-mono text-sm">
             <p className=" left-0 top-0 lg:static lg:w-auto text-2xl">Davi Andrade</p>
             <p className=" left-0 top-0  lg:static lg:w-auto text-xl">Programador FrontEnd</p>
           </div>
-          <div className="z-10">
+          <div className="z-10 w-30 grid col-start-4">
             <Image width={60} height={600} alt="Theme" src={"/dark.png"} />
           </div>
         </header>
 
         <Sidebar />
 
-        <div className="z-10 w-full flex justify-end">
-          <article className="font-mono text-sm text-lg">
+        <div className="grid justify-end col-start-4">
+          <article className="z-10 font-mono text-sm">
             <p className=" w-[150px]">
               {
                 "Graduando em Análise e Desenvolvimento de Sistemas e, além da faculdade, estudo programação online para aprimorar continuamente minhas habilidades. Antes do meu primeiro 'Hello World', fiz trabalhos voluntários e estive à frente do protagonismo estudantil no ensino médio."
