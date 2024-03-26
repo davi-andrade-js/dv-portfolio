c0923863d3348e6b3113e38680523b8ec6c42384
Davi <dv.andrade04@gmail.com>
2024-03-25 22:15:34 -0300

    change projects flex direction

--- src/app/(routes)/projects/page.tsx
+++ src/app/(routes)/projects/page.tsx
@@ -4,7 +4,7 @@ import Sidebar from "../../components/Sidebar";
 export default function Projects() {
   return (
     <>
-      <div className="flex flex-col justify-between h-full">
+      <div className="flex flex-row justify-between h-full">
         <Sidebar activeSection={"projects"} />
         <ProjectsList />
       </div>
