5a35b49ee7a50d4f246f6a06f26d3dc3ceedffd7
Davi <dv.andrade04@gmail.com>
2024-04-09 11:23:51 -0300

    update resume

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -38,7 +38,7 @@ export default function RootLayout({
       </Head>
       <body className={inter.className}>
         <LoadingScreen />
-        <div className="noise"></div>
+        {/* <div className="noise"></div> */}
         <section className="z-10 m-5 mb-0 flex h-[91vh] flex-col rounded-xl p-2 md:m-7 md:p-2 lg:m-8 lg:mb-0 lg:p-5">
           <MovingLights />
           <Header />
