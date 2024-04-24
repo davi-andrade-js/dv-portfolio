b8353869188de58630cafb5d31ab2e39277b538c
Davi <dv.andrade04@gmail.com>
2024-04-24 18:47:34 -0300

    fix projects responsiveness on mobile

--- src/app/(routes)/projects/page.tsx
+++ src/app/(routes)/projects/page.tsx
@@ -13,87 +13,83 @@ export default function ProjectsList() {
 
   return (
     <>
-      <div className="fadeIn font-poppinsRegular mt-8 flex h-full w-full flex-row items-end justify-end overflow-hidden md:mt-0 md:pt-0 lg:w-3/5">
-        <ScrollArea className="scroll h-full md:w-3/4 md:pr-4">
-          {projects.map((project) => (
-            <div className="flex flex-col" key={project.name}>
-              {projects.indexOf(project) !== 0 ? (
-                <div
-                  key={project.name}
-                  className={
-                    "badge m-2 w-full border-transparent md:my-5 md:border"
-                  }
-                ></div>
-              ) : (
-                <></>
-              )}
-
+      <ScrollArea className="scroll fadeIn font-poppinsRegular mt-8 flex h-full items-end md:mt-0 md:w-[31rem] md:pr-4 md:pt-0 lg:w-[36rem]">
+        {projects.map((project) => (
+          <div className="flex flex-col" key={project.name}>
+            {projects.indexOf(project) !== 0 ? (
               <div
-                className="card md:card-none md:hover:card md: flex h-fit w-full cursor-default flex-col items-end space-x-5 space-y-3 rounded-xl p-4 transition-all md:flex-row md:items-start md:space-y-0 md:p-2"
-                onMouseEnter={() => setProjectOnHover(project.name)}
-                onMouseLeave={() => setProjectOnHover("")}
-              >
-                <div className=" flex w-full flex-col items-end justify-between space-y-2  md:w-[90%] md:space-y-4">
-                  <div className="flex w-full flex-col items-end transition-all">
-                    <header className="font-poppinsMedium flex w-full flex-row justify-between">
-                      <div className="flex flex-row items-center space-x-3">
-                        {project.pinned ? (
-                          <Star
-                            className="h-4 w-4 md:h-5 md:w-5"
-                            color="var(--title-color)"
-                            fill="var(--title-color)"
-                          />
-                        ) : (
-                          <></>
-                        )}
-                        <div className=" text-base">{project.year}</div>
-                      </div>
+                key={project.name}
+                className={
+                  "badge m-2 w-full border-transparent md:my-5 md:border"
+                }
+              ></div>
+            ) : (
+              <></>
+            )}
 
-                      <div className="flex text-end text-lg">
-                        {project.name}
-                      </div>
-                    </header>
-
-                    <div className="text-secondary text-right text-base transition-all">
-                      <p className="w-72 transition-all md:w-96">
-                        {project.description}
-                      </p>
+            <div
+              className="card md:card-none md:hover:card flex h-fit w-full cursor-default flex-col items-end space-x-5 space-y-3 rounded-xl p-4 transition-all md:w-[24rem] md:flex-row md:items-start md:space-y-0 md:p-2 lg:w-full"
+              onMouseEnter={() => setProjectOnHover(project.name)}
+              onMouseLeave={() => setProjectOnHover("")}
+            >
+              <div className=" flex w-full flex-col items-end justify-between space-y-2 md:w-[90%] md:space-y-4">
+                <div className="flex w-full flex-col items-end transition-all">
+                  <header className="font-poppinsMedium flex w-full flex-row justify-between">
+                    <div className="flex flex-row items-center space-x-3">
+                      {project.pinned ? (
+                        <Star
+                          className="h-4 w-4 md:h-5 md:w-5"
+                          color="var(--title-color)"
+                          fill="var(--title-color)"
+                        />
+                      ) : (
+                        <></>
+                      )}
+                      <div className=" text-base">{project.year}</div>
                     </div>
-                  </div>
 
-                  <div className="flex flex-row flex-wrap justify-end gap-1 md:gap-2">
-                    {project.technologies.map((tech) => (
-                      <div key={tech}>
-                        <Badge className="badge text-xs transition-all">
-                          <p className="transition-all">{tech}</p>
-                        </Badge>
-                      </div>
-                    ))}
-                  </div>
+                    <div className="flex text-end text-lg">{project.name}</div>
+                  </header>
 
-                  <ProjectsLink
-                    repositoryUrl={project.repositoryUrl}
-                    liveUrl={project.liveUrl}
-                    projectOnHover={projectOnHover}
-                  />
+                  <div className="text-secondary text-right text-base transition-all">
+                    <p className="w-72 transition-all md:w-96">
+                      {project.description}
+                    </p>
+                  </div>
                 </div>
 
-                <div className="w-fit">
-                  <Image
-                    key={project.screenshot}
-                    src={project.screenshot}
-                    alt="Preview do projeto"
-                    loading="lazy"
-                    width={200}
-                    height={120}
-                    className="rounded-lg border-2 border-zinc-400"
-                  />
+                <div className="flex flex-row flex-wrap justify-end gap-1 md:gap-2">
+                  {project.technologies.map((tech) => (
+                    <div key={tech}>
+                      <Badge className="badge text-xs transition-all">
+                        <p className="transition-all">{tech}</p>
+                      </Badge>
+                    </div>
+                  ))}
                 </div>
+
+                <ProjectsLink
+                  repositoryUrl={project.repositoryUrl}
+                  liveUrl={project.liveUrl}
+                  projectOnHover={projectOnHover}
+                />
+              </div>
+
+              <div className="md:min-w-28 lg:min-w-32">
+                <Image
+                  key={project.screenshot}
+                  src={project.screenshot}
+                  alt="Preview do projeto"
+                  loading="lazy"
+                  width={200}
+                  height={120}
+                  className="rounded-lg border-2 border-zinc-400"
+                />
               </div>
             </div>
-          ))}
-        </ScrollArea>
-      </div>
+          </div>
+        ))}
+      </ScrollArea>
     </>
   );
 }
