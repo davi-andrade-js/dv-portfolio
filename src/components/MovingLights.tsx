0c65d3bb189e21490b6b8bf6496004d7c2a5ca99
Davi <dv.andrade04@gmail.com>
2024-05-02 17:57:57 -0300

    chage moving lights animation

--- src/components/MovingLights.tsx
+++ src/components/MovingLights.tsx
@@ -1,8 +1,37 @@
-export default function MovingLights() {
+'use client';
+
+import { useEffect, useState } from 'react';
+
+const MovingLights = () => {
+  const [endAnimation, setEndAnimation] = useState(false);
+
+  useEffect(() => {
+    setTimeout(() => {
+      setEndAnimation(true);
+    }, 8000);
+  });
+
   return (
-    <div className="motion-area ease m-[20px] h-[91vh] justify-center rounded-xl lg:m-[32px]">
-      <div className="bg-shape1 light-one bg-blur absolute h-[800px] w-[800px] rounded-full opacity-50"></div>
-      <div className="bg-shape2 light-two bg-blur absolute h-[800px] w-[800px] rounded-full opacity-50"></div>
+    <div
+      className={`motion-area ease m-[20px] h-[91vh] rounded-xl lg:m-[32px] ${endAnimation && 'animate-pulse'}`}
+    >
+      <div className="absolute left-[-90vw] md:left-px">
+        <div className="bg-shape1 light-one bg-blur xl:left-[10vw] top-[55vh] md:top-[30vh] absolute rounded-full"></div>
+        <div className="bg-shape2 light-two bg-blur xl:left-[10vw] top-[55vh] md:top-[30vh] absolute rounded-full"></div>
+      </div>
     </div>
   );
+};
+
+export default MovingLights;
+
+{
+  /* <div className="motion-area ease m-[20px] h-[91vh] duration-[4000ms] rounded-xl lg:m-[32px]">
+      <div className="absolute left-[10vw] ">
+        <span className="bg-shape1 light-one top-[55vh] md:top-[30vh] bg-blur absolute rounded-full"></span>
+      </div>
+      <div className="absolute left-[10vw] ">
+        <span className="bg-shape2 light-two top-[55vh] md:top-[30vh] bg-blur absolute rounded-full"></span>
+      </div>
+    </div> */
 }
